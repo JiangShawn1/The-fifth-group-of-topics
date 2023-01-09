e71a9f1b15283749d7e4a1c0ccccdf919076fdfb
“Tony” <tunglin.yang1@gmail.com>
2023-01-09 16:41:12 +0800

    20230109 QuestionTypes

--- 專題/Controllers/CommonQuestionController.cs
+++ 專題/Controllers/CommonQuestionController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using 專題.Models.EFModels;
 
 namespace 專題.Controllers
 {
     public class CommonQuestionController : Controller
     {
-        // GET: CommonQuestion
-        public ActionResult CommonQuestions()
+		private AppDbContext db = new AppDbContext();
+		// GET: CommonQuestion
+		public ActionResult CommonQuestions()
         {
             return View();
         }
@@ -17,9 +19,24 @@ namespace 專題.Controllers
 		{
 			return View();
 		}
-		public ActionResult QuestionTypes()
+		public ActionResult QuestionTypes(int? QuestionId, string questiontypes)
 		{
-			return View();
+			ViewBag.QuestionTypes = GetQuestionTypes(QuestionId);
+			ViewBag.QuestionType1 = questiontypes;
+			var data = db.QuestionTypes.Include(x => x.Category);
+			if (QuestionId.HasValue) data = data.Where(p => p.Category.Id == categoryId.Value);
+			if (string.IsNullOrEmpty(productName) == false) data = data.Where(p => p.Name.Contains(productName));
+			return View(data.ToList());
+		}
+		private IEnumerable<SelectListItem> GetQuestionTypes(int? QuestionId)
+		{
+			var items = db.QuestionTypes
+				.Select(c => new SelectListItem
+				{ Value = c.Id.ToString(), Text = c.QuestionType1, Selected = (QuestionId.HasValue && c.Id == QuestionId.Value) })
+				.ToList()
+				.Prepend(new SelectListItem { Value = string.Empty, Text = "請選擇" });
+
+			return items;
 		}
 	}
 }
