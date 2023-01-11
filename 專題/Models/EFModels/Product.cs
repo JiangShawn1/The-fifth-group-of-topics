5c1e5c89f3e4eec24bd65135dbedd5c58b67cdbf
SeanLin <sean61104@gmail.com>
2023-01-11 15:56:27 +0800

    完成 Products 驗證

--- 專題/Models/EFModels/Product.cs
+++ 專題/Models/EFModels/Product.cs
@@ -17,20 +17,26 @@ namespace 專題.Models.EFModels
 
         public int Id { get; set; }
 
+        [Required, Display(Name = "品牌名稱")]
         public int Brand_Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "請勿超過50個字")]
+        [Display(Name = "商品名稱")]
+        [Required(ErrorMessage = "請輸入名稱")]
         public string ProductName { get; set; }
 
-        [Required]
-        [StringLength(500)]
+        [Required(ErrorMessage = "請輸入商品介紹")]
+        [StringLength(500, ErrorMessage = "請勿超過500個字")]
+        [Display(Name = "商品介紹")]
         public string ProductIntroduce { get; set; }
-
+        [Required]
+        [Display(Name = "商品顏色")]
         public int Color_Id { get; set; }
-
+        [Required(ErrorMessage = "請輸入商品價錢")]
+        [Display(Name = "商品價錢")]
         public int Price { get; set; }
-
+        [Required(ErrorMessage = "請上傳商品圖片")]
+        [Display(Name = "商品圖片")]
         public int ProductsImages_Id { get; set; }
 
         public virtual Brand Brand { get; set; }
