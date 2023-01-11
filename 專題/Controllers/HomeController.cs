e111009e1e9314edde68c239f64b9c38fba4cdfe
SeanLin <sean61104@gmail.com>
2023-01-11 14:15:18 +0800

    Add VM

--- 專題/Controllers/HomeController.cs
+++ 專題/Controllers/HomeController.cs
@@ -6,25 +6,25 @@ using System.Web.Mvc;
 
 namespace 專題.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
